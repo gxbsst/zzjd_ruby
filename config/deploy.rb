c8d0e221c35706565deaf81026498368f3299dbf
gxbsst <gxbsst@gmail.com>
2015-10-03 20:55:35 +0800

    将sidekiq变成linked_files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :sidekiq_options, -> { "-C #{current_path}/config/sidekiq.yml" }
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/sidekiq.yml}
 set :linked_dirs, %w{log}
 #set :default_env, { path: "~/.rvm/rubies/jruby-1.7.8/bin/ruby:$PATH" }
 
