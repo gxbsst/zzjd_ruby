5215d2f21c12a3c05fb1f6f34e5c647dd64e3899
gxbsst <gxbsst@gmail.com>
2015-10-05 10:17:12 +0800

    增加NC设备管理。

--- app/models/equipments/nc.rb
+++ app/models/equipments/nc.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+
 module Equipments
   module Nc
     class Nc1
@@ -10,19 +11,45 @@ module Equipments
       #     43 => 1 || 0# 数据是否有效 1 为 无效， 0为有效
       # }
       READ_NC_NO_ADD = 0 # 为IP地址
-      READ_VALIDATE_ADD  = 43 # 1 = invalid, 0 = valid
       READ_FIRST_STATE_ADD = 1
       READ_SECOND_STATE_ADD = 2
+      READ_VALIDATE_ADD  = 43 # 1 = invalid, 0 = valid
       WRITE_RUNNING_ADD = 9 #1 为运行(读到是1，马上写0) M-FIM 信号
 
-      attr_accessor :no, :valid_code, :run, :state_1, :state_2, :action
+      attr_accessor :no, :valid_code, :run, :state_1, :state_2, :action, :door, :button, :jig, :robot
+
+      # Usage:
+      # Equipments::Nc::Nc1.build(action: 'in')
+      # Equipments::Nc::Nc1.build(action: 'out')
+      
+      def self.build(values = {})
+        self.new(values)
+      end
 
       def initialize(values = {})
-        @state_1 = values[:state_1]
-        @state_2 = values[:state_2]
-        @action = values[:action]
-        @valid_code = values[:valid_code]
+        sync(values)
       end
+
+      def sync(values)
+        @action = values[:action]
+        begin
+          ModBus::TCPClient.new(Settings.nc.nc1.ip, 502) do |cl|
+            cl.with_slave(1) do |slave|
+              @no, @state_1, @state_2 = slave.holding_registers[READ_NC_NO_ADD..READ_SECOND_STATE_ADD]
+              @valid_code = slave.holding_registers[READ_VALIDATE_ADD]
+            end
+          end
+          @door = Equipments::Nc::Door.build
+          @jig = Equipments::Nc::Jig.build
+          @robot =  Equipments::Nc::robot.build
+        rescue ModBus::Errors::ModBusTimeout => e
+          Rails.logger.info("*"*100)
+          Rails.logger.info("IP地址是不是设置错了啊, Modbus无法连接!!!")
+          Rails.logger.info("*"*100)
+        end
+        self
+      end
+
 
       # 00
       # 正在执行
@@ -43,6 +70,9 @@ module Equipments
         self.state_1 == 0 && self.state_2 == 1
       end
 
+      alias_method :door_can_close?, :jig_can_close?
+      alias_method :door_can_open?, :jig_can_open?
+
       # 10
       # 等待加工, 可以发M-FIN信号执行
       def can_start?
@@ -58,31 +88,137 @@ module Equipments
       end
     end
 
-    class Button < Nc1
+    class Button
       WRITE_START_ADD = 7  # 控制启动按钮, 写1 读到是1，马上写0
       WRITE_CANCEL_ADD = 8  # 1急停 0是取消急停（需要人工复位)
+      WRITE_OPEN_DOOR_ADD = 10  # 1 开门， 0 关门
       attr_accessor :start_state, :cancel_state
 
-      def do_start(nc)
-        @start_state = 1 # write address 7
-        @start_state = 0 # write address 7
+      def self.build
+       self.new
       end
 
+      def do_start(nc)
+        if nc.can_start?
+          Modbus.write(Settings.nc.nc1.ip, WRITE_START_ADD, 1)
+          Modbus.write(Settings.nc.nc1.ip, WRITE_START_ADD, 0)
+        end
+      end
+
+      alias_method :switch, :do_start
+
       def do_cancel(nc)
-        @cancel_state = 1 # write address 8
+        @cancel_state = 1 # TODO: write address 8
+      end
+
+      def open_door(nc)
+        if nc.door_can_open?
+          Modbus.write(Settings.nc.nc1.ip, WRITE_OPEN_DOOR_ADD, 1)
+        end
+      end
+
+      def close_door(nc)
+        if nc.door_can_close?
+          Modbus.write(Settings.nc.nc1.ip, WRITE_OPEN_DOOR_ADD, 0)
+        end
       end
     end
 
-    class Jig <  Nc1# 夹具
+    class Jig # 夹具
       READ_CLOSED_ADD = 3 # 夹具是否已经加紧
       READ_OPENED_ADD = 4 # 夹具是否已经加紧
+      WRITE_OPENED_ADD = 9 # M-FIN
+      attr_accessor :opened_state, :closed_state
+
+      def self.build
+        self.new
+      end
+
+      def initialize
+        sync
+      end
+
+      def sync
+        begin
+          ModBus::TCPClient.new(Settings.nc.nc1.ip, 502) do |cl|
+            cl.with_slave(1) do |slave|
+              @jig_closed_status, @jig_opened_status= slave.holding_registers[READ_CLOSED_ADD..READ_OPENED_ADD]
+            end
+          end
+        rescue ModBus::Errors::ModBusTimeout => e
+          Rails.logger.info("*"*100)
+          Rails.logger.info("IP地址是不是设置错了啊, Modbus无法连接!!!")
+          Rails.logger.info("*"*100)
+        end
+        self
+      end
+
+      def do_open(nc, button)
+        if nc.jig_can_open?
+          button.switch(nc)
+          self.opened_state = 1 # TODO: 生产环境删除，主要为了测试
+        end
+      end
+
+      def do_close(nc, button)
+        if nc.jig_can_close?
+          button.switch(nc)
+          self.closed_state = 1 # TODO: 生产环境删除，主要为了测试
+        end
+      end
+
+      def opened?
+       self.opened_state == 1
+      end
+
+      def closed?
+        self.opened_state == 1
+      end
     end
 
     class Door < Nc1# 门
-      WRITE_OPERATION_ADD = 10  # 1 开门， 0 关门
       READ_OPENED_ADD = 5 # 0,1
-      READ_OPENED_ADD = 6 # 0,1
-      attr_accessor :state
+      READ_CLOSED_ADD = 6 # 0,1
+      attr_accessor :opened_state, :closed_state
+
+      def self.build
+        self.new
+      end
+
+      def initialize
+        sync
+      end
+
+      def sync
+        begin
+          ModBus::TCPClient.new(Settings.nc.nc1.ip, 502) do |cl|
+            cl.with_slave(1) do |slave|
+              @door_opened_status, @door_closed_status = slave.holding_registers[READ_OPENED_ADD..READ_CLOSED_ADD]
+            end
+          end
+        rescue ModBus::Errors::ModBusTimeout => e
+          Rails.logger.info("*"*100)
+          Rails.logger.info("IP地址是不是设置错了啊, Modbus无法连接!!!")
+          Rails.logger.info("*"*100)
+        end
+        self
+      end
+
+      def do_open(nc, button)
+        button.open_door(nc)
+      end
+
+      def do_close(nc, button)
+        button.close_door(nc)
+      end
+
+      def opened?
+        self.opened_state == 1
+      end
+
+      def closed?
+        self.opened_state == 1
+      end
     end
 
     class Program < Nc1
@@ -90,6 +226,55 @@ module Equipments
       attr_accessor :no
     end
 
+    class Robot
+
+      def self.build
+        self.new 
+      end
+
+      def initialize
+       sync 
+      end
+
+      def sync
+        
+      end
+
+      def do_go
+        
+      end
+
+      def do_leave
+
+      end
+
+      def leaved?
+        true
+      end
+
+      def arrived?
+        true
+      end
+    end
+
+  end
+end
+
+agv_arrived = true
+if agv_arrived
+  nc = Equipments::Nc::Nc1.build(action: 'in')
+  button =  Equipments::Nc::Button.build
+  door =  Equipments::Nc::Door.build
+  jig =  Equipments::Nc::Jig.build
+  robot =  Equipments::Nc::Robot.build
+  button.open_door(nc)
+  button.switch(nc)
+
+  if door.opened?
+    robot.do_go
+  end
+
+  if robot.arrived?
 
   end
 end
--- spec/models/equipments/nc1_spec.rb
+++ spec/models/equipments/nc1_spec.rb
@@ -43,17 +43,73 @@ RSpec.describe Equipments::Nc::Nc1 do
   end
 
   describe Equipments::Nc::Button do
-    let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 1) }
-    let(:button) { Equipments::Nc::Button.new }
 
-    it "do running" do
-      button.do_start nc1
-      expect(button.start_state).to eq(0)
+    context 'nc can start' do
+      let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 1) }
+      let(:button) { Equipments::Nc::Button.new }
+
+      it "do start" do
+        button.do_start nc1
+        expect(button.start_state).to eq(nil)
+      end
     end
 
-    it "do cancel" do
-      button.do_cancel nc1
-      expect(button.cancel_state).to eq(1)
+    context 'nc can not start' do
+      let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 0) }
+      let(:button) { Equipments::Nc::Button.new }
+
+      it "do start" do
+        button.do_start nc1
+        expect(button.start_state).to eq(0)
+      end
+    end
+
+    describe 'cancel' do
+      let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 0) }
+      let(:button) { Equipments::Nc::Button.new }
+
+      it "do cancel" do
+        button.do_cancel nc1
+        expect(button.cancel_state).to eq(1)
+      end
+
+    end
+  end
+
+  describe Equipments::Nc::Jig do
+    let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 1) }
+    let(:jig) { Equipments::Nc::Jig.new }
+    let(:button) { Equipments::Nc::Button.new }
+
+    it 'do open' do
+      expect(nc1).to receive(:jig_can_open?).and_return(true)
+      jig.do_open(nc1, button)
+      expect(jig.opened_state).to eq(1)
+      # expect(jig.opened_state).to eq(1)
+    end
+
+    it 'do close' do
+      expect(nc1).to receive(:jig_can_close?).and_return(true)
+      jig.do_close(nc1, button)
+      expect(jig.closed_state).to eq(1)
+    end
+  end
+
+  describe Equipments::Nc::Door do
+    let(:nc1) { Equipments::Nc::Nc1.new(state_1: 1, state_2: 0, valid_code: 1) }
+    let(:door) { Equipments::Nc::Door.new }
+
+    it 'do open' do
+      expect(nc1).to receive(:jig_can_open?).and_return(true)
+      jig.do_open(nc1, button)
+      expect(jig.opened_state).to eq(1)
+      # expect(jig.opened_state).to eq(1)
+    end
+
+    it 'do close' do
+      expect(nc1).to receive(:jig_can_close?).and_return(true)
+      jig.do_close(nc1, button)
+      expect(jig.closed_state).to eq(1)
     end
   end
 
